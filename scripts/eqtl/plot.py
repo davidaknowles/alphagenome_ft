a76158b0a0f5f8fb55e3c3193ba5ed403183f473
David A Knowles <knowles84@gmail.com>
2026-09-22 13:42:42 -0400

    Generate eQTL benchmark plots

--- scripts/eqtl/plot.py
+++ scripts/eqtl/plot.py
@@ -46,7 +46,7 @@ def main() -> None:
     bin_order = ["0-1kb", "1-10kb", "10-50kb", "50-100kb", "100-250kb", "250-500kb", ">500kb"]
     plot_summary = summary[summary["distance_bin"] != "all"].copy()
     plot_summary["distance_bin"] = pd.Categorical(plot_summary["distance_bin"], categories=bin_order, ordered=True)
-    plot = (ggplot(plot_summary, aes("distance_bin", "auroc", color="model", group="model")) + geom_hline(yintercept=0.5, linetype="dashed", color="grey50") + geom_line() + geom_point() + facet_wrap("~mode") + scale_y_continuous(limits=(0.4, 1.0)) + labs(x="Distance from TSS", y="AUROC", color="Model") + theme_bw() + theme(axis_text_x=element_text(rotation=45, hjust=1)))
+    plot = (ggplot(plot_summary, aes("distance_bin", "auroc", color="model", group="model")) + geom_hline(yintercept=0.5, linetype="dashed", color="#808080") + geom_line() + geom_point() + facet_wrap("~mode") + scale_y_continuous(limits=(0.4, 1.0)) + labs(x="Distance from TSS", y="AUROC", color="Model") + theme_bw() + theme(axis_text_x=element_text(rotation=45, hjust=1)))
     plot.save(args.output_dir / "auroc_by_distance.pdf", width=8, height=4.5, verbose=False)
     plot.save(args.output_dir / "auroc_by_distance.png", width=8, height=4.5, dpi=180, verbose=False)
 
